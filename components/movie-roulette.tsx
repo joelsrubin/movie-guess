a55fb89ae00de4b99bc586a6750ce2c6bc02ba4b
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-10-21 14:13:54 -0400

    update favicon

--- components/movie-roulette.tsx
+++ components/movie-roulette.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useMemo } from "react"
+
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
