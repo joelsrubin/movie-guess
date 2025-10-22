dcec81215a066deb190221a104f26e9e327bd60b
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-10-22 07:37:24 -0400

    themed

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export default function Header({ queueButton }: HeaderProps) {
 	return (
 		<div className="sticky top-0 z-50 max-w-screen flex justify-between items-center border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 px-2">
 			<div className="flex items-center justify-center gap-3">
-				<Film className="w-10 h-10 text-accent" />
+				<Film className="w-10 h-10 text-primary" />
 				<h1 className="sm:text-4xl text-xl md:text-6xl font-bold text-balance">Movie Roulette</h1>
 			</div>
 			<div className="flex items-center gap-3">
