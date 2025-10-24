dc6fade673417315eaa327415d0c2c790f8aab90
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-10-24 01:39:55 -0700

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
 import { Toaster } from "sonner"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -51,6 +52,7 @@ export default function RootLayout({
 						<Toaster position="top-left" />
 					</ThemeProvider>
 					<ReactQueryDevtools initialIsOpen={false} />
+					<SpeedInsights />
 				</QueryClientProvider>
 			</body>
 		</html>
